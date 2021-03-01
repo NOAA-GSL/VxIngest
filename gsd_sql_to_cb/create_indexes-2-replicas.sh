8bb157cb2ae5f528c7d366136cf5e5946859fc83
randy.pierce <randy.pierce@noaa.gov>
2021-03-01 14:47:01 -0700

    added new index to create_indexes-2 replicas

--- gsd_sql_to_cb/create_indexes-2-replicas.sh
+++ gsd_sql_to_cb/create_indexes-2-replicas.sh
@@ -1,3 +1,4 @@
 CREATE INDEX `adv_type_subset_docType_fcstValidEpoch` ON `mdata`(`type`,`docType`,`fcstValidEpoch`) WHERE (`subset`="METAR") WITH {"defer_build":true, "num_replica":2};
 CREATE INDEX `adv_type_subset_docType_version` ON `mdata`(`type`,`docType`,`version`) WHERE (`subset`="METAR") WITH {"defer_build":true, "num_replica":2};
 CREATE INDEX `adv_type_subset_docType_version_fcstValidEpoch` ON `mdata`(`type`,`docType`,`version`,`fcstValidEpoch`) WHERE (`subset`="METAR") WITH {"defer_build":true, "num_replica":2};
+CREATE INDEX `adv_geo_lat_geo_lon_type_docType_subset_version` ON `mdata`((`geo`.`lat`),(`geo`.`lon`)) WHERE ((((`docType` = "station") and (`subset` = "METAR")) and (`version` = "V03")) and (`type` = "DD")){"defer_build":true, "num_replica":2}

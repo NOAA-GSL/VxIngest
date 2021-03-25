17fe1186c15e2b1bb12eaffd5893daf82c35e0a4
randy.pierce <randy.pierce@noaa.gov>
2021-03-25 13:39:05 -0600

    added a new index to support the new CTC document format and added missing builder_type field to ingest_ctcs.json documents.

--- gsd_sql_to_cb/index_creation_scripts/create_indexes-2-replicas.sh
+++ gsd_sql_to_cb/index_creation_scripts/create_indexes-2-replicas.sh
@@ -1,4 +1,5 @@
 
+CREATE INDEX `adv_region_fcstValidEpoch_docType_subset_version_type` ON `mdata`(`region`,`fcstValidEpoch`) WHERE ((((`type` = "DD") and (`docType` = "CTC")) and (`subset` = "METAR")) and (`version` = "V01")) WITH {  "defer_build":true, "num_replica":2};
 CREATE INDEX `idx_type_docType_version_METAR` ON `mdata`(`type`,`docType`,`version`) WHERE (`subset` = "METAR") WITH {  "defer_build":true, "num_replica":2};
 CREATE INDEX `idx_type_docType_version_common` ON `mdata`(`type`,`docType`,`version`) WHERE (`subset` = "COMMON") WITH {  "defer_build":true, "num_replica":2};
 CREATE INDEX `idx_type_docType_version_test` ON `mdata`(`type`,`docType`,`version`) WHERE (`subset` = "TEST") WITH { "defer_build":true, "num_replica":2};

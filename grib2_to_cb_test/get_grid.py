f8be042c919e608c62eb131492d8bec11013b4df
JeffHamiltonNOAA <jeffrey.a.hamilton@noaa.gov>
2021-07-14 12:25:46 -0600

    added logic to handle stations better with max_x and max_y

--- grib2_to_cb_test/get_grid.py
+++ grib2_to_cb_test/get_grid.py
@@ -12,20 +12,20 @@ import pyproj
 
 def getGrid(grib2_file):
     grbs = pygrib.open(grib2_file)
-    grb = grbs[0]
+    grb = grbs[1]
 
     # Find the false origin easting and northing for conversion to lat-lon domain
-    init_projection = pyproj.Proj(grb[0].projparams)
+    init_projection = pyproj.Proj(grb[1].projparams)
     latlon_proj = pyproj.Proj(proj='latlon')
-    lat_0 = grb[0].latitudeOfFirstGridPointInDegrees
-    lon_0=grb[0].longitudeOfFirstGridPointInDegrees
+    lat_0 = grb[1].latitudeOfFirstGridPointInDegrees
+    lon_0=grb[1].longitudeOfFirstGridPointInDegrees
 
     transformer = pyproj.Transformer.from_proj(proj_from=latlon_proj,proj_to=init_projection)
     x, y = transformer.transform(lon_0,lat_0, radians=False)
 
     # Add the proper conversion to 'fool' Proj into setting 0,0 in the lower left corner of the domain
     ## NOTE: It doesn't actually do this, but it will be necessary to find x,y coordinates relative to the lower left corner
-    projection_params = grb[0].projparams
+    projection_params = grb[1].projparams
     projection_params['x_0'] = abs(x)
     projection_params['y_0'] = abs(y)
 
@@ -35,12 +35,16 @@ def getGrid(grib2_file):
     grbs.close()
     return grid_projection
 
-def getSpacing(grib2_file):
+def getAttributes(grib2_file):
     grbs = pygrib.open(grib2_file)
-    grb = grbs[0]
+    grb = grbs[1]
 
     # Get grid spacing (needed to find the proper x,y)
-    spacing = (grb[0].Dx)/1000
+    spacing = (grb[1].Dx)/1000
+
+    #Grab max points in x,y directions
+    max_x = grb[1].Nx
+    max_y = grb[1].Ny
 
     grbs.close()
-    return spacing
+    return spacing, max_x, max_y

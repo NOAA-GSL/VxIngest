7cf1afbca05925562e1841a493af6ce457a9001c
randy.pierce <randy.pierce@noaa.gov>
2021-07-16 09:06:37 -0600

    continue working on bounding box test

--- grib2_to_cb/test/test_metar_station_geo_boundingBox_search.py
+++ grib2_to_cb/test/test_metar_station_geo_boundingBox_search.py
@@ -49,6 +49,15 @@ class TestStationBoundingBoxQuery(unittest.TestCase):
             self.assertTrue (Path(grib2_file).is_file(), "/opt/public/data/grids/hrrr/conus/wrfprs/grib2/2119614000018 Does not exist" )
 
             projection = gg.getGrid(grib2_file)
+            grbs = pygrib.open(grib2_file)
+            grbm = grbs.message(1)
+            lat,lon = grbm.latlons()
+            lat_shape = lat.shape
+            lat_min = lat.min()
+            lat_max = lat.max()
+            lon_shape = lon.shape
+            lon_min = lon.min()
+            lon_max = lon.max()
             spacing, max_x, max_y = gg.getAttributes(grib2_file)
 
             self.assertEqual(projection.description, 'PROJ-based coordinate operation', "projection description: is Not corrrect")
@@ -59,12 +68,12 @@ class TestStationBoundingBoxQuery(unittest.TestCase):
             transformer_reverse = pyproj.Transformer.from_proj(proj_from=in_proj,proj_to=out_proj)
 
             # Grab the lat-lon of the max grid points
-            lon_min, lat_min = transformer_reverse.transform(0,0, radians=False)
-            lon_max, lat_max = transformer_reverse.transform(max_x*spacing,max_y*spacing, radians=False)
+            t_lon_min, t_lat_min = transformer_reverse.transform(0,0, radians=False)
+            t_lon_max, t_lat_max = transformer_reverse.transform(max_x*spacing,max_y*spacing, radians=False)
             #Location objects are specified as a Tuple[SupportsFloat,SupportsFloat] of longitude and latitude respectively
-            top_left = (lon_max,lat_max)
-            bottom_right = (lon_min, lat_min)
-            query = GeoBoundingBoxQuery(top_left, bottom_right)
+            top_left = (lon_min,lat_max)
+            bottom_right = (lon_max, lat_min)
+            query = GeoBoundingBoxQuery(lat_max, lon_min, lat_min, lon_max )
             result = self.cluster.search_query("geo-station", query)
             self.assertEqual( result.metadata().metrics().total_rows(), 3000, "Reported total rows: is not 3000")
         except Exception as e:

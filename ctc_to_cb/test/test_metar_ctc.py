8f2d66d9bb2fb94620c4793ac233a54c28f36460
randy.pierce <randy.pierce@noaa.gov>
2021-08-20 17:20:00 -0600

    add test for classic stations

--- ctc_to_cb/test/test_metar_ctc.py
+++ ctc_to_cb/test/test_metar_ctc.py
@@ -6,8 +6,12 @@ import json
 import os
 import sys
 import unittest
+import yaml
+from pathlib import Path
+from couchbase.cluster import Cluster, ClusterOptions
+from couchbase.auth import PasswordAuthenticator
+from couchbase.search import GeoBoundingBoxQuery, SearchOptions
 from ctc_to_cb.run_ingest_threads import VXIngest
-
 
 class TestCTCBuilderV01(unittest.TestCase):
     """
@@ -15,6 +19,49 @@ class TestCTCBuilderV01(unittest.TestCase):
     This test expects to write to the local output directory /opt/data/ctc_to_cb/output
     so that directory should exist.
     """
+
+    def test_get_stations_geo_search(self):
+        try:
+            cwd = os.getcwd()
+            self.credentials_file = os.environ['HOME'] + '/adb-cb1-credentials'
+            self.assertTrue(Path(self.credentials_file).is_file(),
+                            "credentials_file Does not exist")
+            f = open(self.credentials_file)
+            yaml_data = yaml.load(f, yaml.SafeLoader)
+            host = yaml_data['cb_host']
+            user = yaml_data['cb_user']
+            password = yaml_data['cb_password']
+            options = ClusterOptions(PasswordAuthenticator(user, password))
+            self.cluster = Cluster('couchbase://' + host, options)
+            self.collection = self.cluster.bucket("mdata").default_collection()
+            result = self.cluster.query(
+                """
+                SELECT name,
+                    geo.bottom_right.lat AS br_lat,
+                    geo.bottom_right.lon AS br_lon,
+                    geo.top_left.lat AS tl_lat,
+                    geo.top_left.lon AS tl_lon
+                FROM mdata
+                WHERE type='MD'
+                    AND docType='region'
+                    AND subset='COMMON'
+                    AND version='V01'
+                """)
+            for row in result:
+                result1 = self.cluster.search_query("station_geo", GeoBoundingBoxQuery(
+                    top_left=(row['tl_lon'], row['tl_lat']), bottom_right=(row['br_lon'], row['br_lat']), field="geo"), SearchOptions(fields=["name"], limit=10000))
+                classic_station_id = "MD-TEST:V01:CLASSIC_STATIONS:" + row['name']
+                doc = self.collection.get(classic_station_id.strip())
+                classic_stations = doc.content['stations']
+                classic_stations.sort()
+                stations = []
+                for elem in list(result1):
+                    stations.append(elem.fields['name'])
+                stations.sort()
+                intersection = [value for value in stations if value in classic_stations]
+                self.assertTrue (len(classic_stations) - len(intersection) < 100, "difference between expected and actual greater than 100")
+        except Exception as e:
+            self.fail("TestGsdIngestManager Exception failure: " + str(e))
 
     def test_ctc_builder_hrrr_ops_all_hrrr(self):
         # noinspection PyBroadException

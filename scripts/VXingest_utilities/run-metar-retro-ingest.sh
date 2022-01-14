3dbcad68e7fd3564fe8c433e58c29d8bb3897065
randy.pierce <randy.pierce@noaa.gov>
2022-01-14 16:01:47 -0700

    retro

--- scripts/VXingest_utilities/run-metar-retro-ingest.sh
+++ scripts/VXingest_utilities/run-metar-retro-ingest.sh
@@ -32,7 +32,7 @@ fi
 
 echo "*************************************"
 echo "netcdf obs and stations"
-#rm -rf /data/netcdf_to_cb-retro/output/*
+rm -rf /data/netcdf_to_cb-retro/output/*
 python netcdf_to_cb/run_ingest_threads.py -s /data/netcdf_to_cb-retro/load_specs/load_spec_netcdf_metar_retro_obs_V01.yaml -c ~/adb-cb1-credentials -p /couchbase/xfer/ -m %Y%m%d_%H%M -o /data/netcdf_to_cb-retro/output -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p /data/netcdf_to_cb-retro/output -n 8 -l ${clonedir}/logs-retro
 

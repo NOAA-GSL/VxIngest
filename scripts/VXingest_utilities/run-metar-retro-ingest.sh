b2cbeff653f2b3324256896326c100c48ec335cc
randy.pierce <randy.pierce@noaa.gov>
2022-02-16 17:50:04 -0700

    fix problem with model name in LEGACY documents

--- scripts/VXingest_utilities/run-metar-retro-ingest.sh
+++ scripts/VXingest_utilities/run-metar-retro-ingest.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-echo STARTING 
+echo STARTING
 date
 if [ $# -ne 1 ]; then
         echo "Usage $0 VxIngest_clonedir";
@@ -33,7 +33,7 @@ fi
 echo "*************************************"
 echo "netcdf obs and stations"
 rm -rf /data/netcdf_to_cb-retro/output/*
-python netcdf_to_cb/run_ingest_threads.py -s /data/netcdf_to_cb-retro/load_specs/load_spec_netcdf_metar_retro_obs_V01.yaml -c ~/adb-cb1-credentials -p /couchbase/xfer/ -m %Y%m%d_%H%M -o /data/netcdf_to_cb-retro/output -t8
+python netcdf_to_cb/run_ingest_threads.py -s /data/netcdf_to_cb-retro/load_specs/load_spec_netcdf_metar_legacy_retro_obs_V01.yaml -c ~/adb-cb1-credentials -p /couchbase/xfer/ -m %Y%m%d_%H%M -o /data/netcdf_to_cb-retro/output -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p /data/netcdf_to_cb-retro/output -n 8 -l ${clonedir}/logs-retro
 
 echo "*************************************"

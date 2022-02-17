aaa8047c12e19ce724224b5eafecc1ae5c3b0be3
randy.pierce <randy.pierce@noaa.gov>
2022-02-17 14:05:55 -0700

    typo in script

--- scripts/VXingest_utilities/run-cron-retro-ctcs.sh
+++ scripts/VXingest_utilities/run-cron-retro-ctcs.sh
@@ -29,7 +29,7 @@ fi
 echo "*************************************"
 echo "retro ctc's"
 rm -rf /data/ctc_to_cb-retro/output/*
-python ctc_to_cb/run_ingest_threads.py -s /data/ctc_to_cb/load_specs/load_spec_metar_ctc_retro_V01.yaml  -c ~/adb-cb1-credentials -o /data/ctc_to_cb-retro/output -t8
+python ctc_to_cb/run_ingest_threads.py -s /data/ctc_to_cb/load_specs/load_spec_metar_ctc_legacy_retro_V01.yaml  -c ~/adb-cb1-credentials -o /data/ctc_to_cb-retro/output -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p /data/ctc_to_cb-retro/output -n 8 -l ${clonedir}/logs
 
 echo "FINISHED"

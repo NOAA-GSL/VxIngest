799e32f70155640732c0d9d950a11c6006c65e69
randy.pierce <randy.pierce@noaa.gov>
2022-04-12 10:42:35 -0600

    remove legacy entries from run_cron

--- scripts/VXingest_utilities/run-cron.sh
+++ scripts/VXingest_utilities/run-cron.sh
@@ -26,20 +26,22 @@ if [ ! -d "${HOME}/logs" ]; then
         mkdir ${HOME}/logs
 fi
 
+echo "--------"
 echo "*************************************"
 echo "netcdf obs and stations"
 outdir="/data/netcdf_to_cb/output/${pid}"
 python ${clonedir}/netcdf_to_cb/run_ingest_threads.py -s /data/netcdf_to_cb/load_specs/load_spec_netcdf_metar_obs_V01.yaml -c ~/adb-cb1-credentials -p /public/data/madis/point/metar/netcdf -m %Y%m%d_%H%M -o $outdir -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 
-echo "*************************************"
-echo "netcdf legacy obs and stations"
-outdir="/data/netcdf_to_cb-legacy/output/${pid}"
-python ${clonedir}/netcdf_to_cb/run_ingest_threads.py -s /data/netcdf_to_cb/load_specs/load_spec_netcdf_metar_legacy_obs_V01.yaml -c ~/adb-cb1-credentials -p /public/data/madis/point/metar/netcdf -m %Y%m%d_%H%M -o $outdir -t8
-${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
+#echo "*************************************"
+#echo "netcdf legacy obs and stations"
+#outdir="/data/netcdf_to_cb-legacy/output/${pid}"
+#python ${clonedir}/netcdf_to_cb/run_ingest_threads.py -s /data/netcdf_to_cb/load_specs/load_spec_netcdf_metar_legacy_obs_V01.yaml -c ~/adb-cb1-credentials -p /public/data/madis/point/metar/netcdf -m %Y%m%d_%H%M -o $outdir -t8
+#${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 
-echo "*************************************"
+echo "--------"
 echo "models"
+echo "*************************************"
 echo "hrrr_ops"
 outdir="/data/grib2_to_cb/hrrr_ops/output/${pid}"
 python ${clonedir}/grib2_to_cb/run_ingest_threads.py -s /data/grib2_to_cb/load_specs/load_spec_grib_metar_hrrr_ops_V01.yaml -c ~/adb-cb1-credentials -p /public/data/grids/hrrr/conus/wrfprs/grib2 -m %y%j%H%f -o $outdir -t8
@@ -51,18 +53,20 @@ outdir="/data/grib2_to_cb/rap_ops_130/output/${pid}"
 python ${clonedir}/grib2_to_cb/run_ingest_threads.py -s /data/grib2_to_cb/load_specs/load_spec_grib_metar_rap_ops_130_V01.yaml -c ~/adb-cb1-credentials -p /public/data/grids/rap/iso_130/grib2 -m %y%j%H%f -o $outdir -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 
-echo "*************************************"
+echo "--------"
 echo "ctc's"
+echo "*************************************"
 outdir="/data/ctc_to_cb/output/${pid}"
 python ${clonedir}/ctc_to_cb/run_ingest_threads.py -s /data/ctc_to_cb/load_specs/load_spec_metar_ctc_V01.yaml  -c ~/adb-cb1-credentials -o $outdir -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 
-echo "*************************************"
-echo "legacy ctc's"
-outdir="/data/ctc_to_cb-legacy/output/${pid}"
-python ${clonedir}/ctc_to_cb/run_ingest_threads.py -s /data/ctc_to_cb/load_specs/load_spec_metar_ctc_legacy_V01.yaml  -c ~/adb-cb1-credentials -o $outdir -t8
-${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
+#echo "*************************************"
+#echo "legacy ctc's"
+#outdir="/data/ctc_to_cb-legacy/output/${pid}"
+#python ${clonedir}/ctc_to_cb/run_ingest_threads.py -s /data/ctc_to_cb/load_specs/load_spec_metar_ctc_legacy_V01.yaml  -c ~/adb-cb1-credentials -o $outdir -t8
+#${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 
+echo "--------"
 echo "*************************************"
 echo "update metadata"
 ${clonedir}/mats_metadata_and_indexes/metadata_files/update_ceiling_mats_metadata.sh ~/adb-cb1-credentials

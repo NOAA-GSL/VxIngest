24514e7c37c19f1ad67ffacd189578bc7a24b271
randy.pierce <randy.pierce@noaa.gov>
2022-05-04 12:20:38 -0600

    minor script change

--- scripts/VXingest_utilities/run-cron.sh
+++ scripts/VXingest_utilities/run-cron.sh
@@ -30,6 +30,7 @@ echo "--------"
 echo "*************************************"
 echo "netcdf obs and stations"
 outdir="/data/netcdf_to_cb/output/${pid}"
+mkdir $outdir
 python ${clonedir}/netcdf_to_cb/run_ingest_threads.py -s /data/netcdf_to_cb/load_specs/load_spec_netcdf_metar_obs_V01.yaml -c ~/adb-cb1-credentials -p /public/data/madis/point/metar/netcdf -m %Y%m%d_%H%M -o $outdir -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 
@@ -44,12 +45,14 @@ echo "models"
 echo "*************************************"
 echo "hrrr_ops"
 outdir="/data/grib2_to_cb/hrrr_ops/output/${pid}"
+mkdir $outdir
 python ${clonedir}/grib2_to_cb/run_ingest_threads.py -s /data/grib2_to_cb/load_specs/load_spec_grib_metar_hrrr_ops_V01.yaml -c ~/adb-cb1-credentials -p /public/data/grids/hrrr/conus/wrfprs/grib2 -m %y%j%H%f -o $outdir -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 
 echo "*************************************"
 echo "rap_ops_130"
 outdir="/data/grib2_to_cb/rap_ops_130/output/${pid}"
+mkdir $outdir
 python ${clonedir}/grib2_to_cb/run_ingest_threads.py -s /data/grib2_to_cb/load_specs/load_spec_grib_metar_rap_ops_130_V01.yaml -c ~/adb-cb1-credentials -p /public/data/grids/rap/iso_130/grib2 -m %y%j%H%f -o $outdir -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 
@@ -58,6 +61,7 @@ echo "ctc's"
 echo "*************************************"
 echo "hrrr_ops rap_ops_130"
 outdir="/data/ctc_to_cb/output/${pid}"
+mkdir $outdir
 python ${clonedir}/ctc_to_cb/run_ingest_threads.py -s /data/ctc_to_cb/load_specs/load_spec_metar_ctc_V01.yaml  -c ~/adb-cb1-credentials -o $outdir -t8
 ${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p $outdir -n 8 -l ${clonedir}/logs
 

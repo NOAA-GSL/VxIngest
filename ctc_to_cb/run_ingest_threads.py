097628c3fd0a0555c8bc734fc52fee9c726fa7c8
randy.pierce <randy.pierce@noaa.gov>
2022-05-04 13:51:42 -0600

    still cb_connection stuff

--- ctc_to_cb/run_ingest_threads.py
+++ ctc_to_cb/run_ingest_threads.py
@@ -219,6 +219,7 @@ class VXIngest(object):
             load_spec_file = LoadYamlSpecFile({"spec_file": self.spec_file})
             # read in the load_spec file
             self.load_spec = dict(load_spec_file.read())
+            self.get_credentials(self.load_spec)
             # stash the first_last_params because the builder will need to detrmine
             # if it needs to check for the latest validEpoch from the database (first_epoch == 0)
             self.load_spec["first_last_params"] = self.first_last_params

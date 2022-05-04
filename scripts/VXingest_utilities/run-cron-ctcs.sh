313247443c82af2933b78587b17c3d7e9a7202df
mats.gsl <mats.gsl@noaa.gov>
2022-05-04 18:31:39 +0000

    run script for just ctcs

---
+++ scripts/VXingest_utilities/run-cron-ctcs.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env sh
+
+echo STARTING 
+date
+if [ $# -ne 1 ]; then
+        echo "Usage $0 VxIngest_clonedir";
+        exit 1
+fi
+clonedir=$1
+
+if [ "$(whoami)" != "amb-verif" ]; then
+        echo "Script must be run as user: amb-verif"
+        exit 255
+fi
+
+source ${HOME}/vxingest-env/bin/activate
+
+cd ${clonedir} && export PYTHONPATH=`pwd`
+gitroot=$(git rev-parse --show-toplevel)
+if [ "$gitroot" != "$(pwd)" ];then
+        echo "$(pwd) is not a git root directory: Usage $0 VxIngest_clonedir"
+        exit
+fi
+
+if [ ! -d "${HOME}/logs" ]; then
+        mkdir ${HOME}/logs
+fi
+
+#echo "*************************************"
+#echo "netcdf obs and stations"
+#rm -rf /data/netcdf_to_cb/output/*
+#python netcdf_to_cb/run_ingest_threads.py -s /data/netcdf_to_cb/load_specs/load_spec_netcdf_metar_obs_V01.yaml -c ~/adb-cb1-credentials -p /public/data/madis/point/metar/netcdf -m %Y%m%d_%H%M -o /data/netcdf_to_cb/output -t8
+#${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p /data/netcdf_to_cb/output -n 8 -l ${clonedir}/logs
+
+#echo "*************************************"
+#echo "models"
+#echo "hrrr_ops"
+#rm -rf /data/grib2_to_cb/hrrr_ops/output/*
+#python grib2_to_cb/run_ingest_threads.py -s /data/grib2_to_cb/load_specs/load_spec_grib_metar_hrrr_ops_V01.yaml -c ~/adb-cb1-credentials -p /public/data/grids/hrrr/conus/wrfprs/grib2 -m %y%j%H%f -o /data/grib2_to_cb/hrrr_ops/output -t8
+#${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p /data/grib2_to_cb/hrrr_ops/output -n 8 -l ${clonedir}/logs
+
+#echo "*************************************"
+#echo "rap_ops_130"
+#rm -rf /data/grib2_to_cb/rap_ops_130/output/*
+#python grib2_to_cb/run_ingest_threads.py -s /data/grib2_to_cb/load_specs/load_spec_grib_metar_rap_ops_130_V01.yaml -c ~/adb-cb1-credentials -p /public/data/grids/rap/iso_130/grib2 -m %y%j%H%f -o /data/grib2_to_cb/rap_ops_130/output -t8
+#${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p /data/grib2_to_cb/rap_ops_130/output -n 8 -l ${clonedir}/logs
+
+echo "*************************************"
+echo "legacy ctc's"
+rm -rf /data/ctc_to_cb-legacy/output/*
+python ctc_to_cb/run_ingest_threads.py -s /data/ctc_to_cb/load_specs/load_spec_metar_ctc_legacy_V01.yaml  -c ~/adb-cb1-credentials -o /data/ctc_to_cb-legacy/output -t8
+${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p /data/ctc_to_cb-legacy/output -n 8 -l ${clonedir}/logs
+
+#echo "*************************************"
+#echo "ctc's"
+#rm -rf /data/ctc_to_cb/output/*
+#python ctc_to_cb/run_ingest_threads.py -s /data/ctc_to_cb/load_specs/load_spec_metar_ctc_V01.yaml  -c ~/adb-cb1-credentials -o /data/ctc_to_cb/output -t8
+#${clonedir}/scripts/VXingest_utilities/import_docs.sh -c ~/adb-cb1-credentials -p /data/ctc_to_cb/output -n 8 -l ${clonedir}/logs
+
+echo "*************************************"
+echo "update metadata"
+${clonedir}/mats_metadata_and_indexes/metadata_files/update_ceiling_mats_metadata.sh ~/adb-cb1-credentials
+
+echo "FINISHED" 
+date

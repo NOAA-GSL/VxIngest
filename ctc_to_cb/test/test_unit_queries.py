348844f42d374fff31c2584fab3f56720c79b002
randy.pierce <randy.pierce@noaa.gov>
2022-10-31 11:46:11 -0600

    unit tests for queries

--- ctc_to_cb/test/test_unit_queries.py
+++ ctc_to_cb/test/test_unit_queries.py
@@ -4,8 +4,6 @@ from pathlib import Path
 import yaml
 from couchbase.cluster import Cluster, ClusterOptions, QueryOptions
 from couchbase_core.cluster import PasswordAuthenticator
-import inspect
-
 
 def connect_cb():
     """
@@ -46,76 +44,104 @@ def connect_cb():
     except Exception as _e:  # pylint:disable=broad-except
         assert False, f"test_unit_queries Exception failure connecting: {_e}"
 
-
-def test_map_station_query_no_let(request):
+def test_epoch_fcstLen_model(request):
     """test"""
     try:
         _name = request.node.name
-        _statement = open("./builder_common/test/map_station_query_no_let.n1ql").read()
+        _expected_time = 1.0
+        _statement = open("./ctc_to_cb/test/test_epoch_fcstLen_model.n1ql").read()
         result = connect_cb()["cluster"].query(_statement, QueryOptions(metrics=True))
         elapsed_time = result.metadata().metrics().elapsed_time().total_seconds()
         print(f"{_name}: elapsed_time is {elapsed_time}")
         assert result is not None, "{_name}: result is None"
         assert len(result.errors) == 0, f"{_name}: result has errors{result.errors}"
-        assert elapsed_time < 10, "{_name}: elasped_time greater than 10 {elapsed_time}"
+        assert elapsed_time < _expected_time, f"{_name}: elasped_time greater than {_expected_time} {elapsed_time}"
     except Exception as _e:  # pylint:disable=broad-except
         assert False, f"{_name} Exception failure: {_e}"
 
-
-def test_map_station_query(request):
+def test_epoch_fcstLen_obs(request):
     """test"""
     try:
         _name = request.node.name
-        _statement = open("./builder_common/test/map_station_query.n1ql").read()
+        _expected_time = 0.01
+        _statement = open("./ctc_to_cb/test/test_epoch_fcstLen_obs.n1ql").read()
         result = connect_cb()["cluster"].query(_statement, QueryOptions(metrics=True))
         elapsed_time = result.metadata().metrics().elapsed_time().total_seconds()
         print(f"{_name}: elapsed_time is {elapsed_time}")
         assert result is not None, "{_name}: result is None"
         assert len(result.errors) == 0, f"{_name}: result has errors{result.errors}"
-        assert elapsed_time < 10, "{_name}: elasped_time greater than 10 {elapsed_time}"
+        assert elapsed_time < _expected_time, f"{_name}: elasped_time greater than {_expected_time} {elapsed_time}"
     except Exception as _e:  # pylint:disable=broad-except
         assert False, f"{_name} Exception failure: {_e}"
 
-
-def test_stations_fcst_valid_epoch(request):
+def test_forecast_valid_epoch(request):
     """test"""
     try:
         _name = request.node.name
-        _statement = open("./builder_common/test/stations_fcst_valid_epoch.n1ql").read()
+        _expected_time = 0.2
+        _statement = open("./ctc_to_cb/test/test_forecast_valid_epoch.n1ql").read()
         result = connect_cb()["cluster"].query(_statement, QueryOptions(metrics=True))
         elapsed_time = result.metadata().metrics().elapsed_time().total_seconds()
         print(f"{_name}: elapsed_time is {elapsed_time}")
         assert result is not None, "{_name}: result is None"
         assert len(result.errors) == 0, f"{_name}: result has errors{result.errors}"
-        assert elapsed_time < 10, "{_name}: elasped_time greater than 10 {elapsed_time}"
+        assert elapsed_time < _expected_time, f"{_name}: elasped_time greater than {_expected_time} {elapsed_time}"
     except Exception as _e:  # pylint:disable=broad-except
         assert False, f"{_name} Exception failure: {_e}"
 
-
-def test_stations_get_file_list_grib2(request):
+def test_get_region_lat_lon(request):
     """test"""
     try:
         _name = request.node.name
-        _statement = open("./builder_common/test/get_file_list_grib2.n1ql").read()
+        _expected_time = 0.01
+        _statement = open("./ctc_to_cb/test/test_get_region_lat_lon.n1ql").read()
         result = connect_cb()["cluster"].query(_statement, QueryOptions(metrics=True))
         elapsed_time = result.metadata().metrics().elapsed_time().total_seconds()
         print(f"{_name}: elapsed_time is {elapsed_time}")
         assert result is not None, "{_name}: result is None"
         assert len(result.errors) == 0, f"{_name}: result has errors{result.errors}"
-        assert elapsed_time < 10, "{_name}: elasped_time greater than 10 {elapsed_time}"
+        assert elapsed_time < _expected_time, f"{_name}: elasped_time greater than {_expected_time} {elapsed_time}"
     except Exception as _e:  # pylint:disable=broad-except
         assert False, f"{_name} Exception failure: {_e}"
 
-def test_stations_get_file_list_netcdf(request):
+def test_get_stations(request):
     """test"""
     try:
         _name = request.node.name
-        _statement = open("./builder_common/test/get_file_list_netcdf.n1ql").read()
+        _expected_time = 1.5
+        _statement = open("./ctc_to_cb/test/test_get_stations.n1ql").read()
         result = connect_cb()["cluster"].query(_statement, QueryOptions(metrics=True))
         elapsed_time = result.metadata().metrics().elapsed_time().total_seconds()
         print(f"{_name}: elapsed_time is {elapsed_time}")
         assert result is not None, "{_name}: result is None"
         assert len(result.errors) == 0, f"{_name}: result has errors{result.errors}"
-        assert elapsed_time < 10, "{_name}: elasped_time greater than 10 {elapsed_time}"
+        assert elapsed_time < _expected_time, f"{_name}: elasped_time greater than {_expected_time} {elapsed_time}"
     except Exception as _e:  # pylint:disable=broad-except
         assert False, f"{_name} Exception failure: {_e}"
+
+def test_get_threshold_descriptions(request):
+    """test"""
+    try:
+        _name = request.node.name
+        _expected_time = 0.01
+        _statement = open("./ctc_to_cb/test/test_get_threshold_descriptions.n1ql").read()
+        result = connect_cb()["cluster"].query(_statement, QueryOptions(metrics=True))
+        elapsed_time = result.metadata().metrics().elapsed_time().total_seconds()
+        print(f"{_name}: elapsed_time is {elapsed_time}")
+        assert result is not None, "{_name}: result is None"
+        assert len(result.errors) == 0, f"{_name}: result has errors{result.errors}"
+        assert elapsed_time < _expected_time, f"{_name}: elasped_time greater than {_expected_time} {elapsed_time}"
+    except Exception as _e:  # pylint:disable=broad-except
+        assert False, f"{_name} Exception failure: {_e}"
+
+
+# geo search for stations - currently not implemented
+# _result1 = self.load_spec["cluster"].search_query(
+#     "station_geo",
+#     GeoBoundingBoxQuery(
+#         top_left=(_boundingbox["tl_lon"], _boundingbox["tl_lat"]),
+#         bottom_right=(_boundingbox["br_lon"], _boundingbox["br_lat"]),
+#         field="geo",
+#     ),
+#     SearchOptions(fields=["name"], limit=10000),
+# )

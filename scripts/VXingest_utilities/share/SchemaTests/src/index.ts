dfba22447512921c77bb54ae72b0d47090918f9c
gopa-noaa <gopa.padmanabhan@.noaa.gov>
2022-12-05 21:17:27 +0000

    try catch for JSON errors

--- scripts/VXingest_utilities/share/SchemaTests/src/index.ts
+++ scripts/VXingest_utilities/share/SchemaTests/src/index.ts
@@ -5,14 +5,16 @@ import { CbTimeSeries0 } from "./CbTimeSeries0";
 
 async function main()
 {
-    let cbu : CbUpload = new CbUpload();
+    let cbu: CbUpload = new CbUpload();
     await cbu.init('/Users/gopa.padmanabhan/mats-settings/configurations/dev/settings/cb-ceiling/settings.json', 'vxdata');
     // await cbu.init('/home/gopa/mats-settings/configurations/dev/settings/cb-ceiling/settings.json', 'vxdata');
     // await cbu.uploadJsonLinesDefault();
-    await cbu.uploadJsonLines('/Users/gopa.padmanabhan/scratch/mdatatest/mdatatest_export_gopa.json', 1000);
+    // await cbu.uploadJsonLines('/Users/gopa.padmanabhan/scratch/mdatatest/mdatatest_export_gopa.json', 1000);
+    // await cbu.uploadJsonLines('/scratch/mdata_export.json', 0);
+    await cbu.uploadJsonLines('/scratch/mdatatest/xaa.json', 0);
     // await cbu.uploadJsonLines('/scratch/mdatatest/mdatatest_export_gopa.json', 10);
     // await cbu.jsonLinesExamine0();
-    
+
     let ts0 = new CbTimeSeries0();
     // await ts0.doTimeSeriesQuery0(cbu.bucket, './queries/timeseries_mdata_3.sql');
     // await ts0.doTimeSeriesQueryA(cbu.bucket, './queries/timeseries_mdata_3_model.sql', './queries/timeseries_mdata_3_obs.sql');

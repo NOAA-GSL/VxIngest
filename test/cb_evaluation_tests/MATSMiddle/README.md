b6aca43673e7a0deeb301b89a60d4f4d0cfbb65a
gopa-noaa <gopa.padmanabhan@.noaa.gov>
2023-05-10 15:07:38 +0000

    removing MATSMiddle

--- test/cb_evaluation_tests/MATSMiddle/README.md
+++
@@ -1,3 +0,0 @@
-npm install
-npm start
-npm run upload
--- test/cb_evaluation_tests/MATSMiddle/sqlTemplates/tmpl_get_N_stations_mfve_obs.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    {{stationNamesList}}
-FROM
-    `vxdata`._default.METAR AS obs
-WHERE
-    type = "DD"
-    AND docType = "obs"
-    AND version = "V01"
-    AND fcstValidEpoch = {{fcstValidEpoch}}

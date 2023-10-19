0f7e5c027ee1add74b01c08685e9320871a6ba99
randy.pierce <randy.pierce@noaa.gov>
2023-10-19 15:32:36 -0600

    changed JOB pec id in setup

--- ctc_to_cb/test/test_unit_metar_ctc.py
+++ ctc_to_cb/test/test_unit_metar_ctc.py
@@ -14,7 +14,7 @@ def setup_ingest():
         _vx_ingest.cb_credentials = _vx_ingest.get_credentials(_vx_ingest.load_spec)
         _vx_ingest.connect_cb()
         _vx_ingest.load_spec["ingest_document_ids"] = _vx_ingest.collection.get(
-            "JOB-TEST:V01:METAR:CTC:SUM:MODEL:HRRR_RAP_130"
+            "JOB-TEST:V01:METAR:CTC:CEILING:MODEL:OPS"
         ).content_as[dict]["ingest_document_ids"]
         _vx_ingest.load_spec["ingest_documents"] = {}
         for _id in _vx_ingest.load_spec["ingest_document_ids"]:

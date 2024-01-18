1a7bc53dfcccd913744bb20d4c33d55add20bee1
gopa-noaa <gopa.padmanabhan@.noaa.gov>
2024-01-18 20:11:43 +0000

    removed unused SQLs

--- meta_update_middleware/sqls/deleteModelMetadata.sql
+++
@@ -1,9 +0,0 @@
-delete FROM
-    {{vxDBTARGET}}
-WHERE
-    type = 'MD'
-    AND docType = 'matsGui'
-    AND subset = 'COMMON'
-    AND version = 'V01'
-    AND app = '{{vxAPP}}'
-    AND META().id = 'MD:matsGui:{{vxAPP}}:{{vxMODEL}}:COMMON:V01'

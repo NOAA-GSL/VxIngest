9580f982a97caf73a95f1d3bafdc6f0de40117f6
randy.pierce <randy.pierce@noaa.gov>
2025-05-15 13:56:53 -0600

    sepearting test cases and classes for RAOB grib builders

---
+++ src/vxingest/grib2_to_cb/raob_model_native_builder.py
@@ -0,0 +1,44 @@
+import sys
+
+from vxingest.grib2_to_cb.grib_builder import GribModelBuilderV01
+from vxingest.grib2_to_cb.raob_grib_model_builder import RaobGribModelBuilder
+
+
+class RaobModelNativeBuilderV01(RaobGribModelBuilder):
+    """This is the builder for model data that is ingested from grib2 NATIVE levels files.
+    It is a concrete builder specifically for the model raob data that are organized based
+    on the models preset vertical levels. This varies quite a bit from model to model
+    and is dependent on the configuration set up before the model runs.
+    This builder is a subclass of the GribModelBuilderV01 class.
+    The primary differences between the RaobModelNativeBuilderV01 and the
+    RaobModelPressureLevelBuilderV01 are the handlers that derive the pressure level.
+    The pressure level needs to be interpolated according to a specific algorithm along with
+    the corresponding variable values.
+
+
+    Args:
+        load_spec (Object): The load spec used to init the parent
+        ingest_document (Object): the ingest document
+        number_stations (int, optional): the maximum number of stations to process (for debugging). Defaults to sys.maxsize.
+
+        Note: The entry point for this class is the build_document method from the ancestor class GribBuilder in grib_builder.py.
+        The RaobModelPressureLevelBuilderV01 is a subclass of the RaobGribModelBuilder class.
+        The RaobGribModelBuilder is a subclass of the GribModelBuilderV01 class.
+        The GribModelBuilderV01 is a subclass of the GribBuilder class.
+        The GribBuilder class is the base class for all grib builders and is a subclass of the
+        AbstractGribBuilder class.
+        The AbstractGribBuilder class is the base class for all builders.
+    """
+
+    def __init__(
+        self,
+        load_spec,
+        ingest_document,
+        number_stations=sys.maxsize,
+    ):
+        GribModelBuilderV01.__init__(
+            self,
+            load_spec,
+            ingest_document,
+            number_stations=number_stations,
+        )
---
+++ tests/vxingest/grib2_to_cb/test_int_raob_model_grib.py
@@ -0,0 +1,292 @@
+"""
+integration tests for grib builder for upperair model data.
+This test expects to find a valid grib file in the local directory /opt/public/data/grids/hrrr/conus/wrfprs/grib2.
+This test expects to write to the local output directory /opt/data/grib_to_cb/output so that directory should exist.
+21 196 14 000018 %y %j %H %f  treating the last 6 decimals as microseconds even though they are not.
+these files are two digit year, day of year, hour, and forecast lead time (6 digit ??)
+"""
+
+import json
+import math
+import os
+from datetime import timedelta
+from multiprocessing import Queue
+from pathlib import Path
+
+import pytest
+import yaml
+from couchbase.auth import PasswordAuthenticator
+from couchbase.cluster import Cluster
+from couchbase.options import ClusterOptions, ClusterTimeoutOptions
+
+from vxingest.grib2_to_cb.run_ingest_threads import VXIngest
+
+cb_connection = {}
+
+
+def stub_worker_log_configurer(queue: Queue):
+    """A stub to replace log_config.worker_log_configurer"""
+    pass
+
+
+def connect_cb():
+    """
+    create a couchbase connection and maintain the collection and cluster objects.
+    """
+    if cb_connection:
+        return cb_connection
+    else:
+        credentials_file = os.environ["CREDENTIALS"]
+        assert Path(credentials_file).is_file() is True, (
+            f"*** credentials_file file {credentials_file} can not be found!"
+        )
+        with Path(credentials_file).open(encoding="utf-8") as _f:
+            _yaml_data = yaml.load(_f, yaml.SafeLoader)
+        cb_connection["host"] = _yaml_data["cb_host"]
+        cb_connection["user"] = _yaml_data["cb_user"]
+        cb_connection["password"] = _yaml_data["cb_password"]
+        cb_connection["bucket"] = _yaml_data["cb_bucket"]
+        cb_connection["collection"] = _yaml_data["cb_collection"]
+        cb_connection["scope"] = _yaml_data["cb_scope"]
+
+        timeout_options = ClusterTimeoutOptions(
+            kv_timeout=timedelta(seconds=25), query_timeout=timedelta(seconds=120)
+        )
+        options = ClusterOptions(
+            PasswordAuthenticator(cb_connection["user"], cb_connection["password"]),
+            timeout_options=timeout_options,
+        )
+        cb_connection["cluster"] = Cluster(cb_connection["host"], options)
+        cb_connection["collection"] = (
+            cb_connection["cluster"]
+            .bucket(cb_connection["bucket"])
+            .collection(cb_connection["collection"])
+        )
+        return cb_connection
+
+
+@pytest.mark.integration
+def test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native(tmp_path):
+    """test gribBuilder with one thread.
+    This test verifies the resulting data file against the one that is in couchbase already
+    in order to make sure the calculations are proper."""
+    # 1632412800 fcst_len 1 -> 1632412800 - 1 * 3600 -> 1632409200 September 23, 2021 15:00:00 -> 2126615000001
+    # 1632412800 fcst_len 3 -> 1632412800 - 3 * 3600 -> 1632402000 September 23, 2021 13:00:00 -> 2126613000003
+    # 1632412800 fcst_len 15 -> 1632412800 - 15 * 3600 -> 1632358800 September 22, 2021 19:00:00  ->  (missing)
+    # 1632412800 fcst_len 18 -> 1632412800 - 18 * 3600 -> 1632348000 September 22, 2021 22:00:00 -> 2126522000018 (missing)
+    # 1632420000 September 23, 2021 18:00:00  2126616000018
+    # 1632423600  September 23, 2021 19:00:00 2126617000001
+    # first_epoch = 1634252400 - 10
+    # last_epoch = 1634252400 + 10
+    credentials_file = os.environ["CREDENTIALS"]
+    # remove possible existing DF test documents
+    connect_cb()["cluster"].query("""DELETE
+            FROM `vxdata`._default.RAOB
+            WHERE subset='RAOB'
+            AND type='DF'
+            AND url LIKE "/opt/data/%""")
+
+    log_queue = Queue()
+    vx_ingest = VXIngest()
+    vx_ingest.runit(
+        {
+            "job_id": "JOB-TEST:V01:RAOB:GRIB2:NTV:MODEL:HRRR",
+            "credentials_file": credentials_file,
+            "file_name_mask": "%y%j%H%f",
+            "output_dir": f"{tmp_path}",
+            "threads": 1,
+            "file_pattern": "21287230000[0123456789]?",
+        },
+        log_queue,
+        stub_worker_log_configurer,
+    )
+    # check the output files to see if they match the documents that were
+    # previously created by the real ingest process
+    for _f in tmp_path.glob("*.json"):
+        # read in the output file
+        _json = None
+        with _f.open(encoding="utf-8") as json_file:
+            _json = json.load(json_file)[0]
+        _id = _json["id"]
+        if _id.startswith("LJ"):
+            for _k in _json:
+                assert _k in [
+                    "id",
+                    "subset",
+                    "type",
+                    "lineageId",
+                    "script",
+                    "scriptVersion",
+                    "loadSpec",
+                    "note",
+                ], (
+                    f"test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native LJ failure key {_k} not in {_json.keys()}"
+                )
+            continue
+        _statement = f"select RAOB.* from `{connect_cb()['bucket']}`._default.RAOB where meta().id = '{_id}'"
+        _qresult = connect_cb()["cluster"].query(_statement)
+        result_rows = list(_qresult.rows())
+        assert len(result_rows) > 0, (
+            f"test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native failure test document {_id} not found in couchbase"
+        )
+
+        result = result_rows[0]
+        # assert top level fields
+        keys = _json.keys()
+        for _k in result:
+            assert _k in keys, (
+                f"test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native failure top level key {_k} not in {_json.keys()}"
+            )
+        # assert the units
+        assert result["units"] == _json["units"], (
+            f"test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native failure units {result['units']} != {_json['units']}"
+        )
+        # assert the data
+        for _k in result["data"]:
+            assert _k in _json["data"], (
+                f"test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native failure data key {_k} not in {_json['data'].keys()}"
+            )
+            for _dk in result["data"][_k]:
+                assert _dk in _json["data"][_k], (
+                    f"test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native failure data key {_k}.{_dk} not in {_json['data'][_k].keys()}"
+                )
+                # assert data field matches to 2 decimal places
+                if _dk == "name":
+                    # string compare
+                    assert result["data"][_k][_dk] == _json["data"][_k][_dk], (
+                        f"test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native failure name {result['data'][_k][_dk]} != {_json['data'][_k][_dk]}"
+                    )
+                else:
+                    # math compare
+                    # print(f"result {_k} {_dk} ", result["data"][_k][_dk])
+                    abs_tol = 0.0
+                    if _dk == "Ceiling":
+                        abs_tol = 0.002  # ceiling values don't always have four decimals of resolution
+                    elif _dk == "DewPoint":
+                        abs_tol = 1.0001  # DewPoint only has 3 decimals of precision from pygrib whereas cfgrib is having 4 (or at least the old ingest only had four)
+                        # abs_tol = 0.0001  # DewPoint only has 3 decimals of precision from pygrib whereas cfgrib is having 4 (or at least the old ingest only had four)
+                    elif (
+                        _dk == "RH"
+                    ):  # RH only has one decimal of resolution from the grib file
+                        abs_tol = 1.00001  # not really sure why math.isclose compares out to 5 places but not 6
+                        # abs_tol = 0.00001  # not really sure why math.isclose compares out to 5 places but not 6
+                        # There are no unusual math transformations in the RH handler.
+                    else:
+                        abs_tol = 0.001  # most fields validate between pygrib and cfgrib precisely
+
+                    assert math.isclose(
+                        result["data"][_k][_dk],
+                        _json["data"][_k][_dk],
+                        abs_tol=abs_tol,
+                    ), f"""test_grib_builder_one_thread_file_pattern_hrrr_ops_conus_raob_native failure data not close within {abs_tol}
+                    {_k}.{_dk} {result["data"][_k][_dk]} != {_json["data"][_k][_dk]} within {abs_tol} decimal places."""
+
+
+@pytest.mark.integration
+def test_grib_builder_one_thread_file_pattern_raob_interpolated(tmp_path):
+    """test gribBuilder with one thread for mpas.
+    This test verifies the resulting data file against the one that is in couchbase already
+    in order to make sure the calculations are proper."""
+    credentials_file = os.environ["CREDENTIALS"]
+    # remove possible existing DF test documents
+    connect_cb()["cluster"].query("""DELETE
+            FROM `vxdata`._default.RAOB
+            WHERE subset='RAOB'
+            AND type='DF'
+            AND url LIKE "/opt/data/%""")
+
+    log_queue = Queue()
+    vx_ingest = VXIngest()
+    vx_ingest.runit(
+        {
+            "job_id": "JOB-TEST:V01:RAOB:GRIB2:MODEL:MPAS_physics_dev1",
+            "credentials_file": credentials_file,
+            "file_name_mask": "mpas_phys_dev1_two_%y%j%H_f%f.grib2",
+            "output_dir": f"{tmp_path}",
+            "threads": 1,
+        },
+        log_queue,
+        stub_worker_log_configurer,
+    )
+    # check the output files to see if they match the documents that were
+    # previously created by the real ingest process
+    for _f in tmp_path.glob("*.json"):
+        # read in the output file
+        _json = None
+        with _f.open(encoding="utf-8") as json_file:
+            _json = json.load(json_file)[0]
+        _id = _json["id"]
+        if _id.startswith("LJ"):
+            for _k in _json:
+                assert _k in [
+                    "id",
+                    "subset",
+                    "type",
+                    "lineageId",
+                    "script",
+                    "scriptVersion",
+                    "loadSpec",
+                    "note",
+                ], (
+                    f"test_grib_builder_one_thread_file_pattern_raob_interpolated LJ failure key {_k} not in {_json.keys()}"
+                )
+            continue
+        _statement = f"select RAOB.* from `{connect_cb()['bucket']}`._default.RAOB where meta().id = '{_id}'"
+        _qresult = connect_cb()["cluster"].query(_statement)
+        result_rows = list(_qresult.rows())
+        assert len(result_rows) > 0, (
+            f"test_grib_builder_one_thread_file_pattern_raob_interpolated failure test document {_id} not found in couchbase"
+        )
+
+        result = result_rows[0]
+        # assert top level fields
+        keys = _json.keys()
+        for _k in result:
+            assert _k in keys, (
+                f"test_grib_builder_one_thread_file_pattern_raob_interpolated failure top level key {_k} not in {_json.keys()}"
+            )
+        # assert the units
+        assert result["units"] == _json["units"], (
+            f"test_grib_builder_one_thread_file_pattern_raob_interpolated failure units {result['units']} != {_json['units']}"
+        )
+        # assert the data
+        for _k in result["data"]:
+            assert _k in _json["data"], (
+                f"test_grib_builder_one_thread_file_pattern_raob_interpolated failure data key {_k} not in {_json['data'].keys()}"
+            )
+            for _dk in result["data"][_k]:
+                assert _dk in _json["data"][_k], (
+                    f"test_grib_builder_one_thread_file_pattern_raob_interpolated failure data key {_k}.{_dk} not in {_json['data'][_k].keys()}"
+                )
+                # assert data field matches to 2 decimal places
+                if _dk == "name":
+                    # string compare
+                    assert result["data"][_k][_dk] == _json["data"][_k][_dk], (
+                        f"test_grib_builder_one_thread_file_pattern_raob_interpolated failure name {result['data'][_k][_dk]} != {_json['data'][_k][_dk]}"
+                    )
+                else:
+                    # math compare
+                    # print(f"result {_k} {_dk} ", result["data"][_k][_dk])
+                    abs_tol = 0.0
+                    if _dk == "Ceiling":
+                        abs_tol = 0.002  # ceiling values don't always have four decimals of resolution
+                    elif _dk == "DewPoint":
+                        abs_tol = 1.0001  # DewPoint only has 3 decimals of precision from pygrib whereas cfgrib is having 4 (or at least the old ingest only had four)
+                        # abs_tol = 0.0001  # DewPoint only has 3 decimals of precision from pygrib whereas cfgrib is having 4 (or at least the old ingest only had four)
+                    elif (
+                        _dk == "RH"
+                    ):  # RH only has one decimal of resolution from the grib file
+                        abs_tol = 1.00001  # not really sure why math.isclose compares out to 5 places but not 6
+                        # abs_tol = 0.00001  # not really sure why math.isclose compares out to 5 places but not 6
+                        # There are no unusual math transformations in the RH handler.
+                    else:
+                        abs_tol = 0.001  # most fields validate between pygrib and cfgrib precisely
+
+                    assert math.isclose(
+                        result["data"][_k][_dk],
+                        _json["data"][_k][_dk],
+                        abs_tol=abs_tol,
+                    ), f"""test_grib_builder_one_thread_file_pattern_raob_interpolated failure data not close within {abs_tol}
+                    {_k}.{_dk} {result["data"][_k][_dk]} != {_json["data"][_k][_dk]} within {abs_tol} decimal places."""
+
+

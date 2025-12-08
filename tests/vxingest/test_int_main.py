a380b020f71eaa18ccd900121eaaefecb2cb9d9a
randy.pierce <randy.pierce@noaa.gov>
2025-12-08 14:32:38 -0700

    formatting

--- tests/vxingest/test_int_main.py
+++ tests/vxingest/test_int_main.py
@@ -170,6 +170,7 @@ def test_one_thread_specify_file_pattern_grib2_job_spec_rt(tmp_path: Path):
         # Restore original sys.argv
         sys.argv = original_argv
 
+
 @pytest.mark.integration
 def test_one_thread_specify_file_pattern_grib2_retro_job_spec_rt(tmp_path: Path):
     # Save original sys.argv

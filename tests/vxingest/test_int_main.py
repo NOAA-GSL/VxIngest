35253b34d80ceec13bc860e8c8d16cc4b74dceb2
randy.pierce <randy.pierce@noaa.gov>
2026-05-08 14:46:05 -0600

    format

--- tests/vxingest/test_int_main.py
+++ tests/vxingest/test_int_main.py
@@ -268,8 +268,11 @@ def test_one_thread_specify_file_pattern_grib2_normalized_pressure_job_spec_rt(
         # Restore original sys.argv
         sys.argv = original_argv
 
+
 @pytest.mark.integration
-def test_one_thread_specify_file_pattern_grib2_normalized_pressure_sums_job_spec_rt(tmp_path: Path):
+def test_one_thread_specify_file_pattern_grib2_normalized_pressure_sums_job_spec_rt(
+    tmp_path: Path,
+):
     # Save original sys.argv
     original_argv = sys.argv.copy()
     job_id = "JS:METAR:SUMS:HRRR_OPS:schedule:job:V01"

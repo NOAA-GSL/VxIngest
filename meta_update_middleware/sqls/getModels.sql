5e04104423033eec8913cded3432131a2a79aaed
randy.pierce <randy.pierce@noaa.gov>
2026-08-11 12:54:58 -0600

    removed importer and updater code and cleaned up some old stuff that was lying around.

--- meta_update_middleware/sqls/getModels.sql
+++
@@ -1,7 +0,0 @@
-SELECT DISTINCT RAW (SPLIT(meta().id, ":") [3]) model
-FROM {{vxDBTARGET}}
-WHERE type = "DD"
-    AND version = "V01"
-    AND docType = "{{vxDOCTYPE}}"
-    AND subDocType = "{{vxSUBDOCTYPE}}"
-ORDER BY model
